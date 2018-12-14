3de1232930a28e20d1b0196aeaf3574bd6e440f0
AvanNorth <avannorthy@users.noreply.github.com>
2018-12-14 19:59:49 +0300

    0

--- app/src/main/java/com/example/morty/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/morty/myapplication2/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Нету в проге, но есть в уме", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
