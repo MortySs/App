66992460e4d7862e673059c56ec44c579c7793b6
kamil184 <45402327+kamil184@users.noreply.github.com>
2019-04-17 23:34:14 +0300

    я запутался и устал

--- app/src/main/java/com/example/morty/myapplication2/TestCreateActivity.java
+++ app/src/main/java/com/example/morty/myapplication2/TestCreateActivity.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,62 +29,71 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.annotations.Nullable;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class TestCreateActivity extends AppCompatActivity{
+public class TestCreateActivity extends AppCompatActivity {
     public final ArrayList<String> Questions = new ArrayList<>();
     private EditText name;
     private FirebaseAuth mAuth;
     private TextView mTextMessage;
-    private Button q_create,t_create;
+    private Button q_create, t_create;
     private ProgressBar progressBar;
     final Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_create);
-        mTextMessage = (TextView) findViewById(R.id.tag_name);
-        q_create = (Button)findViewById(R.id.q_add);
+        q_create = (Button) findViewById(R.id.q_add);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        name = (EditText)findViewById(R.id.name_of_test);
-        t_create = (Button)findViewById(R.id.t_create);
-        ListView listView = (ListView)findViewById(R.id.list_tag);
-        ListView questionView = (ListView)findViewById(R.id.test_create_list);
+        name = (EditText) findViewById(R.id.name_of_test);
+        t_create = (Button) findViewById(R.id.t_create);
+        Spinner listView = (Spinner) findViewById(R.id.list_tag);
+        ListView questionView = (ListView) findViewById(R.id.test_create_list);
 
-       final String[] TagNames = getResources().getStringArray(R.array.tag_names);
+        final String[] TagNames = getResources().getStringArray(R.array.tag_names);
 
-       final ExpansionLayout expansionLayout = findViewById(R.id.expansionLayout);
 
-       questionView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-           @Override
-           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-               Intent intent = new Intent(TestCreateActivity.this,TestCreateView.class);
-               intent.putExtra("q_text",Questions.get((int)id));
-               intent.putExtra("number",(int)id+1);
-               startActivity(intent);
-               //Toast.makeText(TestCreateActivity.this, "Нажат вопрос номер " + id, Toast.LENGTH_SHORT).show();
-           }
-       });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        questionView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
-                                    long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                expansionLayout.toggle(true);
-
-                TextView textView = (TextView) itemClicked;
-                mTextMessage.setText( textView.getText().toString());
+                Intent intent = new Intent(TestCreateActivity.this, TestCreateView.class);
+                intent.putExtra("q_text", Questions.get((int) id));
+                intent.putExtra("number", (int) id + 1);
+                startActivity(intent);
+                //Toast.makeText(TestCreateActivity.this, "Нажат вопрос номер " + id, Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = (String) parent.getItemAtPosition(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        listView.setOnItemSelectedListener(itemSelectedListener);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, TagNames);
         listView.setAdapter(adapter);
@@ -143,18 +153,17 @@ public class TestCreateActivity extends AppCompatActivity{
     private void Upd_test(){
         mAuth = FirebaseAuth.getInstance();
         final FirebaseUser cus = mAuth.getCurrentUser();
-        long q_count;
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         final CollectionReference a_draft = db.collection("users").document(cus.getEmail().toString()).collection("tests").document("draft").collection("answers");
         final CollectionReference tests = db.collection("tests"); //document(name.getText().toString());
-        final DocumentReference us = db.collection("users").document(cus.getEmail().toString());
         final DocumentReference other_tests = db.collection("oth_info").document("tests");
+        final DocumentReference us = db.collection("users").document(cus.getEmail().toString());
         final Map<String, Object> data = new HashMap<>();
+        final Map<String, Object> id = new HashMap<>();
+        final Map<String, Object> id1 = new HashMap<>();
         final Map<String, Object> data3 = new HashMap<>();
         final Map<String, Object> us_data = new HashMap<>();
         final Map<String, Object> data1 = new HashMap<>();
-        final Map<String, Object> id_inf = new HashMap<>();
-        final Map<String, Object> test_inf = new HashMap<>();
 
         other_tests.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -162,11 +171,11 @@ public class TestCreateActivity extends AppCompatActivity{
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-                        id_inf.put("last_id",(long)document.get("last_id")+1);
-                        test_inf.put("test_name",name.getText().toString());
-                        Log.d("LOL", "DocumentSnapshot data: " + document.get("last_id")+id_inf.get("test_id"));
-                        other_tests.update(id_inf);
-                        tests.document(id_inf.get("last_id").toString()).set(test_inf);
+                        id.put("last_id",(long)document.get("last_id")+1);
+                        id1.put("test_name",name.getText().toString());
+                        Log.d("LOL", "DocumentSnapshot data: " + document.get("last_id")+id.get("test_id"));
+                        other_tests.update(id);
+                        tests.document(id.get("last_id").toString()).set(id1);
                         for (int i = 0;i<Questions.size();i++){
                             final String count = ""+i;
                             data.put(count, Questions.get(i).toString());
@@ -182,8 +191,8 @@ public class TestCreateActivity extends AppCompatActivity{
                                             if (document.exists()) {
                                                 data1.put(""+k,document.get(""+k).toString());
                                                 data3.put("is_cor_"+k,document.get("is_cor_"+k));
-                                                tests.document(id_inf.get("last_id").toString()).collection("answers").document(count).set(data1);
-                                                tests.document(id_inf.get("last_id").toString()).collection("answers").document(count).update(data3);
+                                                tests.document(id.get("last_id").toString()).collection("answers").document(count).set(data1);
+                                                tests.document(id.get("last_id").toString()).collection("answers").document(count).update(data3);
                                                 Log.d("LOL", "DocumentSnapshot data: " + document.get(""+k));
                                             } else {
                                                 Log.d("LOL", "No such document");
@@ -199,7 +208,7 @@ public class TestCreateActivity extends AppCompatActivity{
                         }
                         data.put("q_count",Questions.size());
 
-                        tests.document(id_inf.get("last_id").toString()).update(data);
+                        tests.document(id.get("last_id").toString()).update(data);
                         us.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                             @Override
                             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -207,7 +216,7 @@ public class TestCreateActivity extends AppCompatActivity{
                                     DocumentSnapshot document = task.getResult();
                                     if (document.exists()) {
                                         us_data.put("name",document.get("name").toString());
-                                        tests.document(id_inf.get("last_id").toString()).update(us_data);
+                                        tests.document(id.get("last_id").toString()).update(us_data);
                                         Log.d("LOL", "DocumentSnapshot data: " + document.get("name"));
                                     } else {
                                         Log.d("LOL", "No such document");
@@ -227,10 +236,4 @@ public class TestCreateActivity extends AppCompatActivity{
             }
         });
     }
-
 }
-
-
-
-
-
