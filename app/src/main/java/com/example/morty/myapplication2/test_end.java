1389e57ed10e671343506eb86f4cb128279f0761
kamil184 <45402327+kamil184@users.noreply.github.com>
2019-04-26 23:50:41 +0300

    почти рейтинг

--- app/src/main/java/com/example/morty/myapplication2/test_end.java
+++ app/src/main/java/com/example/morty/myapplication2/test_end.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,9 +25,10 @@ import java.util.Map;
 public class test_end extends AppCompatActivity {
     private Button btn_end;
     private TextView txt;
-    private int correct_answers_count; //
+    private int correct_answers_count;
     private long q_count;
     private FirebaseAuth auth;
+    private RatingBar ratingBar;
 
     private final Map<String, Object> test_end_data = new HashMap<>();
     @Override
@@ -38,6 +40,10 @@ public class test_end extends AppCompatActivity {
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
         txt = findViewById(R.id.test_result_txt);
         btn_end = findViewById(R.id.test_end_btn);
+        ratingBar = findViewById(R.id.rating);
+        ratingBar.setMax(5);
+        ratingBar.setStepSize(1);
+
         final CollectionReference user_completed = db.collection("users").document(cus.getEmail().toString()).collection("completed");
         final CollectionReference tests = db.collection("tests");
         final Intent intent = getIntent();
@@ -45,7 +51,7 @@ public class test_end extends AppCompatActivity {
         q_count=intent.getLongExtra("q_count",0) ;//("q_count",0);
         test_end_data.put("correct_a_count",correct_answers_count);
         user_completed.document(intent.getStringExtra("Test_id")).set(test_end_data);
-       final DocumentReference cur_test = tests.document(intent.getStringExtra("Test_id"));
+        final DocumentReference cur_test = tests.document(intent.getStringExtra("Test_id"));
         test_end_data.clear();
         cur_test.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -53,6 +59,16 @@ public class test_end extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
+                        if (document.get("rating") != null){
+                            test_end_data.put("rating_sum", (double)ratingBar.getRating() + (double)document.get("rating_sum"));
+                            test_end_data.put("rating_count", 1 + (double)document.get("rating_count"));
+                            test_end_data.put("rating", (double)test_end_data.get("rating_sum") / (double)test_end_data.get("rating_count"));
+                        }else {
+                            test_end_data.put("rating_sum", (double)ratingBar.getRating());
+                            test_end_data.put("rating_count", (double)1);
+                            test_end_data.put("rating", (double)ratingBar.getRating());
+                        }
+                        Log.e("Rating", String.valueOf(ratingBar.getRating()));
                         test_end_data.put("solved_cnt", (long)document.get("solved_cnt")+1);
                         Log.d("testEnd", "onComplete: " + document.get("solved_cnt")+" "+test_end_data.get("solved_cnt"));
                     }
