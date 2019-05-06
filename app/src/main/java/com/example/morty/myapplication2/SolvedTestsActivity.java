4e6a9d0884dc78455e0b642dc805699613e70006
kamil184 <45402327+kamil184@users.noreply.github.com>
2019-05-06 11:33:59 +0300

    решенные тесты

--- app/src/main/java/com/example/morty/myapplication2/SolvedTestsActivity.java
+++ app/src/main/java/com/example/morty/myapplication2/SolvedTestsActivity.java
@@ -40,6 +40,7 @@ import java.util.HashMap;
 
 public class SolvedTestsActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    FirebaseUser cus;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
     private  HashMap<String, String> map;
@@ -52,15 +53,21 @@ public class SolvedTestsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         LayoutInflater inflater = getLayoutInflater();
-        View myView = inflater.inflate(R.layout.my_tests_item, null);
         View myView2 = inflater.inflate(R.layout.my_tests,null);
 
-
         not_auth = (TextView) myView2.findViewById(R.id.not_auth_text);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_tests);
+
         mAuth = FirebaseAuth.getInstance();
-        final FirebaseUser cus = mAuth.getCurrentUser();
+        cus = mAuth.getCurrentUser();
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        questions = (ListView) findViewById(R.id.list);
+
+        updateTests();
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -85,58 +92,6 @@ public class SolvedTestsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.VISIBLE);
-        questions = (ListView) findViewById(R.id.list);
-        updateTests();
-        str = new ArrayList<>() ;
-        final CollectionReference usSolved = db.collection("users").document(cus.getEmail()).collection("completed");
-        usSolved.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()){
-                    for (QueryDocumentSnapshot document : task.getResult()){
-                        str.add(document.getId());
-                    }
-                }
-            }
-        });
-
-    // questions.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-    //     @Override
-    //     public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-    //         LayoutInflater li = LayoutInflater.from(context);
-    //         View promptsView = li.inflate(R.layout.delete_prompt, null);
-    //         AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
-    //         mDialogBuilder.setView(promptsView);
-    //
-    //         final TextView delete = (TextView) promptsView.findViewById(R.id.delete_tv);
-    //         delete.setText("Удалить тест?");
-    //         mDialogBuilder
-    //                 .setCancelable(false)
-    //                 .setPositiveButton("Да",
-    //                         new DialogInterface.OnClickListener() {
-    //                             public void onClick(DialogInterface dialog,int id) {
-    //                                 Log.d("deleting", "onClick: deleted "+arrayList.get(position).get("Test_name"));
-    //
-    //                                 //TODO: удаление теста
-    //                                 InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-    //                                 imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-    //                                 imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-    //                             }
-    //                         })
-    //                 .setNegativeButton("Отмена",
-    //                         new DialogInterface.OnClickListener() {
-    //                             public void onClick(DialogInterface dialog,int id) {
-    //                                 dialog.cancel();
-    //                                 InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-    //                                 imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-    //                                 imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-    //                             }
-    //                         });
-    //         return true;
-    //     }});
     }
 
     void updateTests(){
@@ -151,30 +106,53 @@ public class SolvedTestsActivity extends AppCompatActivity {
 
                         Log.d("MortyList", document.getId() + " => " + document.getData());
                         map = new HashMap<>();
-                        if (str.contains(document.getId())){
-                            DecimalFormat df = new DecimalFormat("#.##");
 
-                            map.put("Test_id",document.getId());
-                            map.put("Test_name",document.get("test_name").toString());
-                            map.put("Q_count", "Вопросов: " + document.get("q_count").toString());
-                            map.put("S_count",document.get("solved_cnt").toString());
-                            if (document.get("rating")!=null) map.put("Rating",df.format(document.get("rating")));
-                            if (document.get("name")!=null) map.put("P_name", document.get("name").toString());
+                        ArrayList<String> Emails = (ArrayList<String>) document.get("solved_emails");
 
-                            arrayList.add(map);
-                            SimpleAdapter adapter = new SimpleAdapter(SolvedTestsActivity.this, arrayList, R.layout.my_tests_item,
-                                    new String[]{"Test_name", "Q_count", "P_name", "S_count", "Rating"},
-                                    new int[]{R.id.test_name, R.id.q_count, R.id.person_name, R.id.solved_count, R.id.test_rating});
-                            questions.setAdapter(adapter);
-                            progressBar.setVisibility(View.GONE);
-                            questions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                @Override
-                                public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
-                                    Intent intent = new Intent(SolvedTestsActivity.this,test_view.class);
-                                    intent.putExtra("Test_id",arrayList.get((int)id).get("Test_id").toString());
-                                    startActivity(intent);
+                        if(Emails != null) {
+                            for (int i = 0; i < Emails.size(); i++) {
+                                if (Emails.get(i).equals(cus.getEmail())) {
+                                    DecimalFormat df = new DecimalFormat("#.##");
+
+                                    map.put("Test_id", document.getId());
+                                    map.put("Test_name", document.get("test_name").toString());
+                                    map.put("Q_count", "Вопросов: " + document.get("q_count").toString());
+                                    map.put("S_count", document.get("solved_cnt").toString());
+                                    if (document.get("rating") != null)
+                                        map.put("Rating", df.format(document.get("rating")));
+                                    if (document.get("name") != null)
+                                        map.put("P_name", document.get("name").toString());
+
+                                    ArrayList<Long> EmailsSolvedCount = (ArrayList<Long>) document.get("emails_solved_count");
+                                    ArrayList<Long> Seconds = (ArrayList<Long>) document.get("seconds");
+                                    ArrayList<Long> Minutes = (ArrayList<Long>) document.get("minutes");
+                                    ArrayList<Long> CorrectAnswersCounts = (ArrayList<Long>) document.get("correct_a_counts");
+
+                                    map.put("R_count", CorrectAnswersCounts.get(i) + " решено верно");
+                                    map.put("Time", "Время " + Minutes.get(i) + ":" + Seconds.get(i));
+                                    String MySCount = EmailsSolvedCount.get(i).toString();
+                                    if (MySCount.endsWith("2") || MySCount.endsWith("3") || MySCount.endsWith("4")) {
+                                        map.put("My_S_count", "Решен вами " + MySCount + " раза");
+                                    } else map.put("My_S_count", "Решен вами " + MySCount + " раз");
+
+                                    arrayList.add(map);
+                                    SimpleAdapter adapter = new SimpleAdapter(SolvedTestsActivity.this, arrayList, R.layout.solved_tests_item,
+                                            new String[]{"Test_name", "Q_count", "P_name", "S_count", "Rating", "R_count", "Time", "My_S_count"},
+                                            new int[]{R.id.test_name, R.id.q_count, R.id.person_name, R.id.solved_count, R.id.test_rating, R.id.my_answers, R.id.my_time, R.id.my_solved_count});
+                                    questions.setAdapter(adapter);
+                                    progressBar.setVisibility(View.GONE);
+                                    questions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                        @Override
+                                        public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+                                            Intent intent = new Intent(SolvedTestsActivity.this, test_view.class);
+                                            intent.putExtra("Test_id", arrayList.get((int) id).get("Test_id").toString());
+                                            startActivity(intent);
+                                        }
+                                    });
+
+                                    break;
                                 }
-                            });
+                            }
                         }
                     }
                 } else {
--- app/src/main/java/com/example/morty/myapplication2/test_end.java
+++ app/src/main/java/com/example/morty/myapplication2/test_end.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,6 +21,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,7 +29,6 @@ public class test_end extends AppCompatActivity {
     private Button btn_end;
     long seconds, minute;
     private TextView txt, time;
-    private int correct_answers_count;
     private long q_count;
     private FirebaseAuth auth;
     private RatingBar ratingBar;
@@ -48,90 +49,142 @@ public class test_end extends AppCompatActivity {
         ratingBar.setMax(5);
         ratingBar.setStepSize(1);
 
+        q_count = getIntent().getLongExtra("q_count", 0);
         seconds = getIntent().getLongExtra("seconds", 0);
         minute = getIntent().getLongExtra("minute", 0);
 
-        final CollectionReference user_completed = db.collection("users").document(cus.getEmail().toString()).collection("completed");
+        //final CollectionReference user_completed = db.collection("users").document(cus.getEmail().toString()).collection("completed");
         final CollectionReference tests = db.collection("tests");
         final Intent intent = getIntent();
-        correct_answers_count = intent.getIntExtra("c_a_c", 0);
-        q_count=intent.getLongExtra("q_count",0) ;//("q_count",0);
-        time.setText("Время: " + minute + " : " + seconds);
-        test_end_data.put("correct_a_count",correct_answers_count);
-        test_end_data.put("minute",minute);
-        test_end_data.put("seconds",seconds);
-        user_completed.document(intent.getStringExtra("Test_id")).set(test_end_data);
         final DocumentReference cur_test = tests.document(intent.getStringExtra("Test_id"));
-        test_end_data.clear();
-        cur_test.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d("Rating", " "+ratingBar.getRating());
-                        test_end_data.put("solved_cnt", (long)document.get("solved_cnt")+1);
-                        Log.d("testEnd", "onComplete: " + document.get("solved_cnt")+" "+test_end_data.get("solved_cnt"));
-                    }
-                }
-            }
-        });
-
-        txt.setText("Вы решили "+correct_answers_count+" / "+q_count );
 
 
+        final long correct_answers_count = intent.getIntExtra("c_a_c", 0);
+        txt.setText("Вы решили "+correct_answers_count+" / "+ q_count );
+        final String CurrentEmail = cus.getEmail();
 
         btn_end.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(test_end.this,MainActivity.class);
-                startActivity(intent);
-                setRating();
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
+                if (ratingBar.getRating() == 0) {
+                    Toast.makeText(getApplicationContext(), "Оцените тест", Toast.LENGTH_LONG).show();
+                } else {
+                    final Intent intent = new Intent(test_end.this, MainActivity.class);
+                    startActivity(intent);
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            cur_test.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot document = task.getResult();
+                                        if (document.exists()) {
 
-                        cur_test.update(test_end_data);
-                    }
+                                            q_count = intent.getLongExtra("q_count", 0);//("q_count",0);
+                                            time.setText("Время: " + minute + " : " + seconds);
+                                            ArrayList<String> Emails;
+                                            ArrayList<Long> EmailsSolvedCount;
+                                            ArrayList<Long> Seconds;
+                                            ArrayList<Long> Minutes;
+                                            ArrayList<Double> Ratings;
+                                            ArrayList<Long> CorrectAnswersCounts;
+                                            if (document.get("ratings") != null) {
+                                                Ratings = (ArrayList<Double>) document.get("ratings");
+                                            } else {
+                                                Ratings = new ArrayList<>();
+                                            }
+                                            if (document.get("solved_emails") != null) {
+                                                Emails = (ArrayList<String>) document.get("solved_emails");
+                                                EmailsSolvedCount = (ArrayList<Long>) document.get("emails_solved_count");
+                                                Seconds = (ArrayList<Long>) document.get("seconds");
+                                                Minutes = (ArrayList<Long>) document.get("minutes");
+                                                CorrectAnswersCounts = (ArrayList<Long>) document.get("correct_a_counts");
+                                            } else {
+                                                Emails = new ArrayList<>();
+                                                EmailsSolvedCount = new ArrayList<>();
+                                                Seconds = new ArrayList<>();
+                                                Minutes = new ArrayList<>();
+                                                CorrectAnswersCounts = new ArrayList<>();
+                                            }
+                                            test_end_data.put("solved_cnt", (long) document.get("solved_cnt") + 1);
+                                            double rating = ratingBar.getRating();
+                                            boolean userSolve = false;
 
-                },2000);
-            }
+                                            for (int i = 0; i < Emails.size(); i++) {
+                                                if (Emails.get(i).equals(CurrentEmail)) {
+                                                    userSolve = true;
+                                                    test_end_data.put("solved_cnt", document.get("solved_cnt"));
+                                                    long sCount = EmailsSolvedCount.get(i);
+                                                    double lastRating = Ratings.get(i);
+                                                    Emails.remove(i);
+                                                    EmailsSolvedCount.remove(i);
+                                                    Seconds.remove(i);
+                                                    Minutes.remove(i);
+                                                    CorrectAnswersCounts.remove(i);
+                                                    Emails.add(CurrentEmail);
+                                                    Log.e("d", String.valueOf(Emails.size()));
+                                                    EmailsSolvedCount.add(sCount + 1);
+                                                    Seconds.add(seconds);
+                                                    Minutes.add(minute);
+                                                    CorrectAnswersCounts.add(correct_answers_count);
+
+                                                    Ratings.remove(i);
+                                                    Ratings.add(rating);
+                                                    if (document.get("rating") != null) {
+                                                        test_end_data.put("rating_sum", (double) document.get("rating_sum") + rating - lastRating);
+                                                        test_end_data.put("rating_count", document.get("rating_count"));
+                                                        test_end_data.put("rating", (double) test_end_data.get("rating_sum") / (long) test_end_data.get("rating_count"));
+                                                    } else {
+                                                        test_end_data.put("rating_sum", rating);
+                                                        test_end_data.put("rating_count", (long) 1);
+                                                        test_end_data.put("rating", rating);
+                                                    }
+
+                                                    break;
+                                                }
+                                            }
+                                            if(!userSolve) {
+                                                Emails.add(CurrentEmail);
+                                                Log.e("d", String.valueOf(Emails.size()));
+                                                EmailsSolvedCount.add((long) 1);
+                                                Seconds.add(seconds);
+                                                Minutes.add(minute);
+                                                CorrectAnswersCounts.add(correct_answers_count);
+
+                                                Ratings.add(rating);
+                                                if (document.get("rating") != null) {
+                                                    test_end_data.put("rating_sum", rating + (double) document.get("rating_sum"));
+                                                    test_end_data.put("rating_count", 1 + (long) document.get("rating_count"));
+                                                    test_end_data.put("rating", (double) test_end_data.get("rating_sum") / (long) test_end_data.get("rating_count"));
+                                                } else {
+                                                    test_end_data.put("rating_sum", rating);
+                                                    test_end_data.put("rating_count", (long) 1);
+                                                    test_end_data.put("rating", rating);
+                                                }
+                                            }
+                                            Log.e("d", String.valueOf(Emails.size()));
+                                            test_end_data.put("solved_emails", Emails);
+                                            test_end_data.put("emails_solved_count", EmailsSolvedCount);
+                                            test_end_data.put("seconds", Seconds);
+                                            test_end_data.put("minutes", Minutes);
+                                            test_end_data.put("correct_a_counts", CorrectAnswersCounts);
+                                            test_end_data.put("ratings", Ratings);
+                                            cur_test.update(test_end_data);
+                                            Log.d("testEnd", "onComplete: " + document.get("solved_cnt") + " " + test_end_data.get("solved_cnt"));
+                                        }
+                                    }
+                                }
+                            });
 
 
-        });
-    }
-    public void setRating(){
-        if (ratingBar.getRating() != 0) {
-            final CollectionReference tests = db.collection("tests");
-            final Intent intent = getIntent();
-            final DocumentReference cur_test = tests.document(intent.getStringExtra("Test_id"));
-
-            cur_test.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
-                            if (document.get("rating") != null) {
-                                test_end_data.put("rating_sum", ratingBar.getRating() + (double) document.get("rating_sum"));
-                                Log.d("Rating", "r s " + test_end_data.get("rating_sum").toString());
-                                test_end_data.put("rating_count", 1 + (long) document.get("rating_count"));
-                                Log.d("Rating", "r c " + test_end_data.get("rating_count").toString());
-                                test_end_data.put("rating", (double) test_end_data.get("rating_sum") / (long) test_end_data.get("rating_count"));
-                                Log.d("Rating", "r " + test_end_data.get("rating").toString());
-                            } else {
-                                test_end_data.put("rating_sum", (double) ratingBar.getRating());
-                                test_end_data.put("rating_count", (long) 1);
-                                test_end_data.put("rating", (double) ratingBar.getRating());
-                            }
-                            Log.d("Rating", " " + ratingBar.getRating());
-                            Log.d("testEnd", "onComplete: " + document.get("solved_cnt") + " " + test_end_data.get("solved_cnt"));
                         }
-                    }
+
+                    }, 2000);
                 }
-            });
-            cur_test.update(test_end_data);
-        }
+
+            }
+        });
     }
 }
